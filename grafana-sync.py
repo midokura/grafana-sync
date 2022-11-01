bc11872dcc352007f37f5858e46092d806e300f6
Florian Castellane <Florian.Castellane@Midokura.com>
2022-11-01 17:26:49 +0900

    feature: save alerts to grafana (known issue: alert exists but is not visible in the web UI)

--- grafana-sync.py
+++ grafana-sync.py
@@ -19,11 +19,25 @@ from datetime import datetime
 import json
 import logging
 import os
+from pprint import pformat
 import re
 import requests
-from sys import argv, exit
+import sys
+from typing import List, Optional
 
-LOGGER = logging.getLogger(argv[0])
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import PythonLexer
+
+LOGGER = logging.getLogger(sys.argv[0])
+
+pprint_size = 120
+
+def pretty_json(json_text: str) -> str:
+    pfmt = pformat(json_text, compact=True, width=pprint_size)
+    if not sys.stdout.isatty():
+        return pfmt
+    return highlight(pfmt, PythonLexer(), TerminalFormatter(bg='dark'))
 
 SOURCE_GRAFANA_TOKEN = os.getenv('SOURCE_GRAFANA_TOKEN')
 SOURCE_GRAFANA_USER = os.getenv('SOURCE_GRAFANA_USER')
@@ -40,7 +54,7 @@ def sanitize(text: str) -> str:
     return re.sub(r'[^a-zA-Z0-9_-]', '_', text)
 
 
-def ls(base_url, session):
+def ls_dashboards(base_url, session):
     """List all Grafana server's dashboards"""
     # https://grafana.com/docs/grafana/latest/developers/http_api/dashboard/#dashboard-search
     request = session.get(base_url + '/api/search?type=dash-db') # type excludes folders
@@ -55,12 +69,11 @@ def get_dashboard(base_url, session, uid: str):
     request = session.get(base_url + f'/api/dashboards/uid/{uid}')
     request.raise_for_status()
     data = request.json()
-    for field in data:
-        LOGGER.debug(f'"{field}": "{data[field]}"')
+    LOGGER.debug('get_dashboard:\n%s', pretty_json(data))
     return data
 
 
-def set_dashboard(base_url, session, data_json, overwrite=False):
+def set_dashboard(base_url, session, data_json, overwrite=False, adapt_uid=False):
     """Create new dashboard in Grafana server"""
     # https://grafana.com/docs/grafana/latest/developers/http_api/dashboard/#create--update-dashboard
     if 'dashboard' not in data_json:
@@ -70,63 +83,146 @@ def set_dashboard(base_url, session, data_json, overwrite=False):
     if 'meta' in dashboard_json:
         # 'meta' is info specific to permissions that can not be set via this API
         del dashboard_json['meta']
-    message = f'Dashboard upload with {argv[0]}'
+    message = f'Dashboard upload with {sys.argv[0]}'
     if USER:
         message += f" by {USER}"
     dashboard_json['Message'] = message
     dashboard_json['overwrite'] = overwrite
-    for field in dashboard_json:
-        LOGGER.debug(f'"{field}": "{dashboard_json[field]}"')
-    request = session.post(base_url + f'/api/dashboards/db', json=dashboard_json)
+    if adapt_uid:
+        if 'dashboard' in dashboard_json and ['panels'] in dashboard_json['dashboard']:
+            for panel in dashboard_json['dashboard']['panels']:
+                if 'datasource' in panel:
+                    name = panel['datasource']['type']
+                    # get datasource uid
+                    print(name)
+                    # panel['datasource']['uid'] = uid
+
+    LOGGER.debug('set_dashboard:\n%s', pretty_json(dashboard_json))
+    request = session.post(base_url + '/api/dashboards/db', json=dashboard_json)
     request.raise_for_status()
 
 
-def get_internal_ruler_rules(base_url, session) -> dict:
-    """ Query internal endpoint to export all alert rules """
-    request = session.get(f"{base_url}/api/ruler/grafana/api/v1/rules")
+def is_alert(data_json):
+    """basic sanity check of a few required fields"""
+    return all(item in data_json for item in {'ruleGroup', 'title'})
+
+
+def set_alert(base_url, session, data_json, overwrite=False, adapt_uid=False) -> Optional[str]:
+    """Create new alert in Grafana server.
+    Requires admin access level
+    https://grafana.com/docs/grafana/v9.1/developers/http_api/alerting_provisioning/#alert-rule
+    """
+    if not is_alert(data_json):
+        raise ValueError("JSON contains no alert rule")
+    if overwrite:
+        raise NotImplementedError('Can not overwrite alerts yet')
+    elif 'uid' in data_json:
+        data = get_alert_rule(base_url, session, data_json['uid'], check_status=False)
+        if is_alert(data):
+            LOGGER.debug('Skipped already existing alert: %s "%s"',
+                data_json['uid'], data_json['title'])
+            return
+    if 'annotations' in data_json:
+        # annotations may reference UIDs of not existing dashboards...
+        # TODO: adapt dashboard uids, or only allow setting the alert with its dashboard maybe
+        del data_json['annotations']
+    # TODO: adapt datasource uids
+    request = session.post(base_url + '/api/v1/provisioning/alert-rules', json=data_json)
+    request.raise_for_status()
+    new_alert = request.json()
+    LOGGER.debug('set_alert:\n%s', pretty_json(new_alert))
+    return new_alert     
+    
+
+def ls_legacy_alerts(base_url, session) -> dict:
+    """Deprecated endpoint"""
+    request = session.get(f"{base_url}/api/alerts")
     request.raise_for_status()
     return request.json()
 
 
-def get_alert_rule(base_url, session, uid: str) -> dict:
-    """ Query public endpoint to export alert rule. UID is required. """
+def get_rule_provisioning_templates(base_url, session) -> dict:
+    request = session.get(f"{base_url}/api/v1/provisioning/templates")
+    data = request.json()
+    LOGGER.debug('get_rule_provisioning_templates:\n%s', pretty_json(data))
+    return data 
+
+def get_internal_ruler_rules(base_url, session) -> dict:
+    """Query internal endpoint to export all alert rules """
+    request = session.get(f"{base_url}/api/ruler/grafana/api/v1/rules")
+    request.raise_for_status()
+    data = request.json()
+    LOGGER.debug('get_internal_ruler_rules:\n%s', pretty_json(data))
+    return data 
+
+
+def ls_alerts(base_url, session) -> set:
+    """there is no "official" endpoint to list alert rules.
+    we do this using an undocumented endpoint to get everything,
+    and then keep only alert uids"""
+    alerts_list = get_internal_ruler_rules(base_url, session)
+    uids = set()
+
+    # we need JSONPath to make this prettier...
+    for group in alerts_list:
+        for alert in alerts_list[group]:
+            for rule in alert.get("rules", {}):
+                alert_uid = rule["grafana_alert"].get("uid")
+                uids.add(alert_uid)
+    return uids
+
+
+def get_alert_rule(base_url, session, uid: str, check_status=True) -> dict:
+    """Query public endpoint to export alert rule. UID is required. 
+    Permission level required: admin"""
     LOGGER.debug(f"Getting alert rule {uid}")
     if not uid:
         raise ValueError("UID cannot be empty!")
     request = session.get(f"{base_url}/api/v1/provisioning/alert-rules/{uid}")
-    request.raise_for_status()
-    return request.json()
+    if check_status:
+        request.raise_for_status()
+    data = request.json()
+    LOGGER.debug('get_alert_rule:\n%s', pretty_json(data))
+    return data
 
 
-def save_dashboard(data: str, folder=None, exist_skip=True):
-    """Save dashboard to a JSON file under a common "dashboard" folder"""
-    dashboard = data['dashboard']
-    filename_pattern = f'{dashboard["title"]}_{dashboard["uid"]}'
-    filename_sanitized = sanitize(filename_pattern) + '.json'
-    if not folder:
-        folder = 'dashboards'
+def save_json(json_text: str, filename, folder, exist_skip=True):
+    """Save JSON file under a common folder"""
     os.makedirs(folder, exist_ok=True)
-    file_path = folder + os.sep + filename_sanitized
+    file_path = folder + os.sep + filename
     if exist_skip and os.path.exists(file_path):
-        LOGGER.info('skipped existing file %s', file_path)
+        LOGGER.info('Skipped existing file %s', file_path)
         return
     with open(file_path, 'w') as f:
-        json.dump(data, f, indent=2)
+        json.dump(json_text, f, indent=2)
 
 
 def save_alert(alert: str, folder=None, exist_skip=True):
-    """Save dashboard to a JSON file under a common "dashboard" folder"""
     filename_pattern = f'{alert["title"]}_{alert["uid"]}'
     filename_sanitized = f"{sanitize(filename_pattern)}.json"
-    if not folder:
-        folder = 'alerts'
-    os.makedirs(folder, exist_ok=True)
-    file_path = folder + os.sep + filename_sanitized
-    if exist_skip and os.path.exists(file_path):
-        LOGGER.info('skipped existing file %s', file_path)
-        return
-    with open(file_path, 'w') as f:
-        json.dump(alert, f, indent=2)
+    return save_json(alert, filename_sanitized, folder or 'alerts', exist_skip)
+
+
+def save_dashboard(data: str, folder=None, exist_skip=True):
+    dashboard = data['dashboard']
+    filename_pattern = f'{dashboard["title"]}_{dashboard["uid"]}'
+    filename_sanitized = f"{sanitize(filename_pattern)}.json"
+    return save_json(dashboard, filename_sanitized, folder or 'dashboards', exist_skip)
+
+
+def load_from_path(path: str) -> List[str]:
+    loaded = []
+    if not os.path.exists(path):
+        raise FileNotFoundError('Source path does not exist: %s', path)
+    if os.path.isdir(path):
+        for item in os.listdir(path):
+            if os.path.isfile(item):
+                loaded.append(load(item))
+    elif os.path.isfile(path):
+        loaded.append(load(path))
+    else:
+        raise ValueError('Source path is not a folder or file: %s', path)
+    return loaded
 
 
 def load(file_name: str):
@@ -152,7 +248,7 @@ def create_token(base_url, session, password, role='Viewer', user='admin'):
             and datetime.strptime(key.get('expiration'), '%Y-%m-%dT%H:%M:%SZ') > datetime.utcnow():
                 LOGGER.warning('There is already an active API key for this role and server.'
                     ' Please try to avoid having many active keys: %s', key)
-    key_name = f"{argv[0]}"
+    key_name = f"{sys.argv[0]}"
     if USER:
         key_name += '-' + USER
     request = basic_auth_session.post(
@@ -170,38 +266,6 @@ def auth_keys(base_url, session):
     request = session.get(base_url + '/api/auth/keys')
     request.raise_for_status()
     print(request.json())
-
-
-def process_alerts(args, source_session=None):
-    """ Execution step from cli for alerts """
-
-    alerts_data = list()
-
-    LOGGER.info("Loading alert rules")
-    if args.source.startswith("http"):
-        alerts_list = get_internal_ruler_rules(args.source, source_session)
-        uids = set() # unique list
-
-        # we need JSONPath to make this prettier...
-        for group in alerts_list.keys():
-            for alert in alerts_list[group]:
-                for rule in alert.get("rules", {}):
-                    uids.add(rule["grafana_alert"].get("uid"))
-
-        LOGGER.debug(f"List of {uids=}")
-
-        for uid in uids:
-            alert = get_alert_rule(args.source, source_session, uid)
-            alerts_data.append(alert)
-    else:
-        raise NotImplementedError('Loading alerts from source is not implemented.')
-
-    LOGGER.info("Saving alert rules")
-    if args.target.startswith("http"):
-        raise NotImplementedError('Saving alerts to target is not implemented.')
-    else:
-        for a in alerts_data:
-            save_alert(a, args.target, exist_skip=not args.force_overwrite)
 
 
 def main():
@@ -255,19 +319,10 @@ def main():
     if 'dashboards' in args.items:
         LOGGER.info('Loading dashboards')
         if args.source.startswith('https://'):
-            for d in ls(args.source, source_session):
+            for d in ls_dashboards(args.source, source_session):
                 source_dashboards.append(get_dashboard(args.source, source_session, d['uid']))
         else:
-            if not os.path.exists(args.source):
-                raise FileNotFoundError('Source path does not exist: %s', args.source)
-            if os.path.isdir(args.source):
-                for item in os.listdir(args.source):
-                    if os.path.isfile(item):
-                        source_dashboards.append(load(item))
-            elif os.path.isfile(args.source):
-                source_dashboards.append(load(args.source))
-            else:
-                raise ValueError('Source path is not a folder or file: %s', args.source)
+            source_dashboards = load_from_path(args.source)
 
         LOGGER.info('Saving dashboards')
         if args.target.startswith('https://'):
@@ -278,12 +333,47 @@ def main():
                 save_dashboard(d, args.target, exist_skip=not args.force_overwrite)
 
     elif 'alerts' in args.items:
-        return process_alerts(args, source_session)
+        LOGGER.info('Loading alerts')
+        source_alerts = list()
+
+        if args.source.startswith("http"):
+            uids = ls_alerts(args.source, source_session)
+
+            LOGGER.debug(f"List of alert {uids=}")
+
+            for uid in uids:
+                alert = get_alert_rule(args.source, source_session, uid)
+                source_alerts.append(alert)
+        else:
+            source_alerts = load_from_path(args.source)
+
+        LOGGER.info("Saving alerts")
+        if args.target.startswith("http"):
+            for a in source_alerts:
+                set_alert(args.target, target_session, a)
+        else:
+            for a in source_alerts:
+                save_alert(a, args.target, exist_skip=not args.force_overwrite)
 
     else:
         raise NotImplementedError('Not implemented... yet!')
 
 
 if __name__ == '__main__':
+    if sys.stdout.isatty():
+        RST = '\u001b[0m'
+        RED = '\u001b[31m'
+        GRN = '\u001b[32m'
+        YLW = '\u001b[33m'
+        MGN = '\u001b[35m'
+        CYA = '\u001b[36m'
+        logging.addLevelName(logging.DEBUG,    CYA + 'DEBUG'    + RST)
+        logging.addLevelName(logging.INFO,     GRN + 'INFO'     + RST)
+        logging.addLevelName(logging.WARNING,  YLW + 'WARNING'  + RST)
+        logging.addLevelName(logging.ERROR,    RED + 'ERROR'    + RST)
+        logging.addLevelName(logging.CRITICAL, MGN + 'CRITICAL' + RST)
+
+        pprint_size = os.get_terminal_size().columns
+    
     logging.basicConfig(level=logging.DEBUG)
-    exit(main())
+    sys.exit(main())
--- with-swagger.py
+++ with-swagger.py
@@ -9,34 +9,56 @@ from sys import argv, exit
 from pprint import pprint
 
 import swagger_client
+from swagger_client.api.datasources_api import DatasourcesApi
 from swagger_client.rest import ApiException
 
 logging.basicConfig(level=logging.INFO)
 
 LOGGER = logging.getLogger(argv[0])
 SOURCE_GRAFANA_TOKEN = os.getenv('SOURCE_GRAFANA_TOKEN')
-GRAFANA_URL = os.getenv('GRAFANA_URL') + '/api'
+GRAFANA_URL = os.getenv('GRAFANA_URL')
 
-# Configure API key authorization: api_key
-configuration = swagger_client.Configuration()
-configuration.debug = True # very verbose!
-configuration.api_key = {'Authorization': SOURCE_GRAFANA_TOKEN}
-configuration.host = GRAFANA_URL
-# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
-configuration.api_key_prefix = {'Authorization': 'Bearer'}
+def make_config():
+    # Configure API key authorization: api_key
+    configuration = swagger_client.Configuration()
+    configuration.debug = True # very verbose!
+    configuration.api_key = {'Authorization': SOURCE_GRAFANA_TOKEN}
+    # Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+    configuration.api_key_prefix = {'Authorization': 'Bearer'}
+    configuration.host = GRAFANA_URL
+    return configuration
+
+configuration = make_config()
+
+# dirty workaround to fix url issue with some endpoints
+configuration_workaround = make_config()
+configuration_workaround.host = GRAFANA_URL + "/api"
 
 # create an instance of the API class
-api_instance = swagger_client.AccessControlApi(swagger_client.ApiClient(configuration))
-search_api = swagger_client.SearchApi(swagger_client.ApiClient(configuration))
-# body = swagger_client.AddBuiltInRoleCommand() # AddBuiltInRoleCommand | 
+# for alerts. there is no method to list alerts currently.
+provisioning_api = swagger_client.ProvisioningApi(swagger_client.ApiClient(configuration))
+# list dashboards
+search_api = swagger_client.SearchApi(swagger_client.ApiClient(configuration_workaround))
+# get dashboards
+dashboards_api = swagger_client.DashboardsApi(swagger_client.ApiClient(configuration))
+# get, list data sources
+datasources_api = swagger_client.DatasourcesApi(swagger_client.ApiClient(configuration_workaround))
 
 if __name__ == '__main__':
     
 
     try:
-        pprint(search_api.search(query=''))
-        # Create a built-in role assignment.
-        #api_response = api_instance.add_builtin_role(body)
-        #pprint(api_response)
+        pprint(search_api.search(type='dash-db'))
+        datasources = datasources_api.get_data_sources()
+        print("\ndatasources")
+        pprint(datasources)
+        for d in datasources:
+            uid = d["uid"]
+            print('\n', 'datasource', d['name'])
+            pprint(datasources_api.get_data_source_by_uid(uid=uid))
+        # pprint(provisioning_api.route_get_policy_tree())
+        # pprint(provisioning_api.route_get_templates())
+                # https://grafana.com/docs/grafana/latest/alerting/migrating-alerts/roll-back/
+
     except ApiException as e:
         LOGGER.exception("Exception when calling API")
